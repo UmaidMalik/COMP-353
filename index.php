a5be2185e7553804cb580e2545851a64974910c3
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-10 15:36:14 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meter http-equiv = "X-UA-Compatible" content = "IE=edge">
+  <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+  <title>index</title>
+</head>
+
+<body>
+  <h1>Welcome!</h1>
+  <p>Choose one of the following links:</p>
+  <a href="./Person/">Person</a>
+
+</body>
+</html>
