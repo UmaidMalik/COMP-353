513c9e8be47dcdd3f6611a0d46bd529687bb6e36
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 17:08:32 -0400

    Update show.php

--- PHW/show.php
+++ PHW/show.php
@@ -1,1 +1,22 @@
-<h1>Hello<h1>
+<?php require_once '../database.php';
+
+$statement = $connection -> prepare("SELECT * FROM fjc353_1.Public_Health_Worker AS PHW WHERE Public_Health_Worker.EID = :EID");
+$statement -> bindParam(":EID", $_GET["EID"]);
+$statement -> execute();
+$phw = $satatement -> fetch(POD: :FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+  <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+  <title><?= $phw["Table PHW"]></title>
+</head>
+<body>
+  <h1><?= $phw["Table PHW"] ?></h1>
+  <p>EID: <?= $phw[EID] ?></p>
+  <p>Medicare Number: <?= $phw[medicare_no] ?></p>
+</body>
+</html>
+
