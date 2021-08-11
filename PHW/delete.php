b4ee1189c32ae57f9ddb17858dbd1e16352e482d
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 17:02:05 -0400

    Update delete.php
    delete for phw added

--- PHW/delete.php
+++ PHW/delete.php
@@ -1,1 +1,9 @@
-<h1>Hello<h1>
+<?php require_once '../database.php';
+
+$statement = $connection->prepare("SELECT * FROM fjc353_1.Public_Health_Worker WHERE Public_Health_Worker.EID = :EID");
+$statement->bindParam(":EID", $_GET["EID"]);
+$statement->execute();
+
+header("Location: .");
+
+?>
