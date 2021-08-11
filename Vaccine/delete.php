d9f1a78a3c02419caa49b5b373bf9125fb9124c8
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 19:59:10 -0400

    Update delete.php

--- Vaccine/delete.php
+++ Vaccine/delete.php
@@ -1,1 +1,10 @@
-<h1>Hello<h1>
+<?php require_once '../database.php';
+
+$statement = $connection->prepare("SELECT * FROM fjc353_1.Vaccine WHERE Vaccine.company = :company");
+$statement->bindParam(":company", $_GET["company"]);
+$statement->execute();
+
+header("Location: .");
+
+?>
+
