aaf6f09a4c19f3167025f950b9f50265d9cb2221
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 17:08:12 -0400

    Update show.php

--- Person/show.php
+++ Person/show.php
@@ -9,7 +9,7 @@ $person = $satatement -> fetch(POD: :FETCH_ASSOC);
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meter http-equiv = "X-UA-Compatible" content = "IE=edge">
+  <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
   <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
   <title><?= $person["Table Person"]></title>
 </head>
