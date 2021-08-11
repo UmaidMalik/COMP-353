fec4c8fb799d9a3be64b6159cd924aad526d5737
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 15:44:47 -0400

    Update delete.php

--- Person/delete.php
+++ Person/delete.php
@@ -1,1 +1,9 @@
-<h1>Hello<h1>
+<?php require_once '../database.php';
+
+$statement = $connection->prepare("SELECT * FROM fjc353_1.Person WHERE Person.medicare_no = :medicare_no");
+$statement->bindParam(":medicare_no", $_GET["medicare_no"]);
+$statement->execute();
+
+header("Location: .");
+
+?>
