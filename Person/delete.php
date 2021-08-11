78f524eddb53a27b823fcbe278874fdd355b6a16
ginibrandon <gini@forsaw.ca>
2021-08-11 16:42:13 -0400

    fixed delete.php

--- Person/delete.php
+++ Person/delete.php
@@ -1,6 +1,6 @@
 <?php require_once '../database.php';
 
-$statement = $connection->prepare("SELECT * FROM fjc353_1.Person WHERE Person.medicare_no = :medicare_no");
+$statement = $connection->prepare("DELETE FROM fjc353_1.Person WHERE Person.medicare_no = :medicare_no");
 $statement->bindParam(":medicare_no", $_GET["medicare_no"]);
 $statement->execute();
 
