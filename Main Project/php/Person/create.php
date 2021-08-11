640d09e379c807dd1862697c7d7f8e9dc7d290da
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 12:06:51 -0400

    Update create.php

--- Main Project/php/Person/create.php
+++ Main Project/php/Person/create.php
@@ -1,1 +1,31 @@
-<h1>Hello<h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+  <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+  <title>create</title>
+</head>
+<body>
+  <form action = "./create.php" method = "post">
+    <lable for = "medicare_no">Medicare Number</label><br>
+    <input type="text" name="medicare_no" id="medicare_no"> <br>
+    <lable for = "first_name">First Name</label><br>
+    <input type="text" name="first_name" id="first_name"> <br>
+    <lable for = "last_name">Last Name</label><br>
+    <input type="text" name="last_name" id="last_name"> <br>
+    <lable for = "date_of_birth">Date Of Birth</label><br>
+    <input type="date" name="date_of_birth" id="date_of_birth"> <br>
+    <lable for = "citizenship">Citizenship</label><br>
+    <input type="text" name="citizenship" id="citizenship"> <br>
+    <lable for = "email">Email</label><br>
+    <input type="text" name="email" id="email"> <br>
+    <lable for = "telephone_no">Phone Number</label><br>
+    <input type="number" name="telephone_no" id="telephone_no"> <br>
+    <lable for = "address">Address</label><br>
+    <input type="text" name="address" id="address"> <br>
+    <lable for = "postal_code">Postal Code</label><br>
+    <input type="text" name="postal_code" id="postal_code"> <br>
+
+</body>
+</html>
