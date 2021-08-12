65e7b0f7cee23f810bc7e18340d63928fa9d0ebe
Nazli Ensafi <n.ensafi87@gmail.com>
2021-08-11 20:45:53 -0400

    Update create.php

--- Province/create.php
+++ Province/create.php
@@ -1,1 +1,33 @@
-<h1>Hello<h1>
+<?php require_once '../database.php';
+
+if(isset($_POST["province"]))
+  {
+$province = $connection->prepare
+("INSERT INTO fjc353_1.Province (province)
+VALUES (:province)";);
+
+$province->bindParam(':province', $_POST["province"]);
+
+if($province->execute())
+  header("Location: .");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+  <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+  <title>create</title>
+</head>
+<body>
+  <form action = "./create.php" method = "post">
+    <label for = "province">Province</label><br>
+    <input type="text" name="province" id="province"> <br>
+    <button type="submit">Add</button>
+  </form>
+  <a href= "./">Back to Province</a>
+
+</body>
+</html>
+
